24fa011ab5d040bbc600b0caec5b838c2c827fee
danielhcg <danihcg.23@gmail.com>
2020-08-04 12:24:19 -0700

    Added background color, reorganized text in what is pop out

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,24 +4,26 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 import javafx.stage.Stage;
 
 // Classes I imported that didn't come with the sample
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.scene.Group;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
+
+import static javafx.scene.paint.Color.rgb;
 
 public class Main extends Application {
 
@@ -49,6 +51,17 @@ public class Main extends Application {
 
         // Setting spacing in between "upperBox" nodes
         //upperBox.setSpacing(10.5);
+
+
+        // Creating a color object
+        Color backgroundColor = rgb(197, 94, 94);
+        // creating a background fill
+        BackgroundFill backgroundFill = new BackgroundFill(backgroundColor, CornerRadii.EMPTY, Insets.EMPTY);
+        // creating a background
+        Background backGround = new Background(backgroundFill);
+        // set background to gridpane
+        gridPane.setBackground(backGround);
+
 
 
         // Adding nodes to pane
@@ -94,11 +107,69 @@ public class Main extends Application {
                             " 5. After four \"Pomodoros\" take a long break\n" +
                             " 6. Repeat to step 1\n" +
                             "\nUSE THE LOOP BUTTON TO DO STEP 1 UNTIL STEP 5 IN A ROW\n";
+
+                    // Declaring paragraph title string variables
+                    String onlinePTimer    = "Online Pomodoro Timer";
+                    String aboutTechnique = "About Pomodoro Technique";
+                    String howToUse       = "How to use Pomodoro/Tomato timers";
+
+                    // Declaring paragraph string variables
+                    String onlinePTimerParagraph = "TomatoTimers is a customizable and easy to use looping " +
+                            "pomodoro timer to boost your efficiency.";
+                    String aboutParagraph = "Pomodoro™ Technique is a time management method developed by " +
+                            "Francesco Cirillo in the late 1980s. This technique use timer to break down " +
+                            "works into a set of intervals separated by breaks. Pomodoro technique increases " +
+                            "productivity by taking short scheduled breaks regularly.";
+                    String howToParagraph = " 1. Decide task to be done set timers to 25 minutes for one Pomodoro" +
+                                            " 2. Work on task until timer is complete" +
+                                            " 3. After timer completion, put a checkmark on to-do list" +
+                                            " 4. Take a 5 minutes short break" +
+                                            " 5. After four Pomodoro take a long break" +
+                                            " 6. Repeat step 1" +
+                                            "USE THE LOOP BUTTON TO DO STEP 1 UNTIL STEP 5 IN A ROW";
+
+                    // Creating text objects and Storing paragraph and title string variables in text objects
+                    Text title1 = new Text(onlinePTimer);
+                    Text title2 = new Text(aboutTechnique);
+                    Text title3 = new Text(howToUse);
+                    Text para1 = new Text(onlinePTimerParagraph);
+                    Text para2 = new Text(aboutParagraph);
+                    Text para3 = new Text(howToParagraph);
+
+                    // Setting the positions of the texts
+                    title1.setX(50);
+                    title1.setY(75);
+                    para1.setX(50);
+                    para1.setY(100);
+
+                    title2.setX(50);
+                    title2.setY(125);
+                    para2.setX(50);
+                    para2.setY(150);
+
+                    title3.setX(50);
+                    title3.setY(175);
+                    para3.setX(50);
+                    para3.setY(200);
+
+
                     whatIsText.setText(description);
 
                     Group whatIsGroup = new Group(whatIsText);
 
-                    whatIsStage.setScene(new Scene(whatIsGroup, 450, 450));
+                    // Creating a new pane and adding description text to it
+                    Pane whatIsPane = new Pane(title1, title2, title3, para1, para2, para3);
+
+                    // Creating a new background fill
+                    BackgroundFill whatIsBackGroundFill = new BackgroundFill(Color.rgb(255, 237, 229), CornerRadii.EMPTY, Insets.EMPTY);
+
+                    // Creating a new background
+                    Background whatIsBackGround = new Background(whatIsBackGroundFill);
+
+                    // Adding background color to pane
+                    whatIsPane.setBackground(whatIsBackGround);
+
+                    whatIsStage.setScene(new Scene(whatIsPane, 798, 543));
                     whatIsStage.show();
 
                 } finally {
@@ -113,6 +184,7 @@ public class Main extends Application {
 
 
         primaryStage.setScene(new Scene(gridPane, 700, 575));
+
         primaryStage.show();
         //sfsdf
     }
